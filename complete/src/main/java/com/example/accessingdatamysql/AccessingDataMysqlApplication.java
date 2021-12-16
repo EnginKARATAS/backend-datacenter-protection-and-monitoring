03f346a38c40db128e52d68e74a50dc98a4ed259
engin.karatas@dvu.com.tr <43602725+EnginKARATAS@users.noreply.github.com>
2021-12-16 23:07:47 +0300

    dht11 controller now getting a time between two LocalDateTime +...
    Data Result classes, folders added to core>utility>results folder

--- complete/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ complete/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -2,10 +2,12 @@ package com.example.accessingdatamysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class AccessingDataMysqlApplication {
 
-	public static void main(String[] args) {SpringApplication.run(AccessingDataMysqlApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataMysqlApplication.class, args);
 	}
 }
